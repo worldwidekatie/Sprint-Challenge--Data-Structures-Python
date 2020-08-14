939852d1030764359d077696c76387217c02aeb8
worldwidekatie <katie.evankodouglas@gmail.com>
2020-08-14 10:03:18 -0600

    names done

--- names/names.py
+++ names/names.py
@@ -19,10 +19,26 @@ for name_1 in names_1:
             duplicates.append(name_1)
 
 end_time = time.time()
+old_end_time = end_time - start_time
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print (f"Original runtime: {old_end_time} seconds")
+
+start_time = time.time()
+# My approach
+for i in names_1:
+    if i in names_2:
+        duplicates.append(i)
+end_time = time.time()
+new_end_time = end_time - start_time
+
+print (f"New runtime: {new_end_time} seconds")
+print(f"Runtime improved by {old_end_time - new_end_time} seconds")
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# original runtime 6.554 Seconds
+# new runtime 1.49 seconds
