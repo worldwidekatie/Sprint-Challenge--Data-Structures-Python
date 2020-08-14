5911c51f50833c1107ea641b07475441cdb8db6f
worldwidekatie <katie.evankodouglas@gmail.com>
2020-08-14 09:52:31 -0600

    ring_buffer passed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,23 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.ls = []
+        self.iter = 0
+        self.iters = []
 
     def append(self, item):
-        pass
+        if len(self.ls) < self.capacity:
+            self.ls.append(item)
+            self.iter += 1
+            self.iters.append(self.iter)
 
+        else:
+            m = min(self.iters)
+            for num, i in enumerate(self.iters):
+                if i == m:
+                    self.ls[num] = item
+                    self.iter += 1
+                    self.iters[num] = self.iter
+    
     def get(self):
-        pass
+        return self.ls
